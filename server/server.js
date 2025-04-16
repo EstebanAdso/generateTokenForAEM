121488e657cd90c1b40ccd371fff30c96b21a65f
Esteban Trujillo <144358984+EstebanAdso@users.noreply.github.com>
2025-04-16 08:51:35 -0500

    fix server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import listAsset from './routes/listAssetRoute.js';
-import { getToken } from './routes/getTokenRoute.js';
 import bodyParser from 'body-parser';
-import copyAsset from './routes/copyAsset.js';
-import downloadAsset from './routes/downloadAssetRoute.js';
-import createFolder from './functions/createFolder.js';
-import listAssetMeta from './routes/listAssetMetadata.js';
-import searchAssetsWithMetadata from './functions/searchAssetMetadata.js';
-import updateMetadata from './routes/updateMetadataRoute.js';
-import { uploadImage } from './functions/uploadImage.js';
 import upload from './middlewares/multerConfig.js';
+import downloadAsset from './routes/downloadAssetRoute.js';
+import listAssetMeta from './routes/listAssetMetadata.js';
+import updateMetadata from './routes/updateMetadataRoute.js';
+import copyAsset from './routes/copyAsset.js';
+import { getToken } from './routes/getTokenRoute.js';
 import deleteAsset from './routes/deleteAssetRoute.js';
+import createFolderAEM from './routes/folderRoute.js';
+import searchAssetMetadata from './routes/searchAssetWithMetadataRoute.js';
+import uploadImageAEM from './routes/uploadImageRoute.js';
 
 // Configuración de Express
 const app = express();
@@ -23,13 +23,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/api/assets/copy', copyAsset);
 app.delete('/api/assets/:path/:name', deleteAsset);
 app.get('/api/download/:aemPath(*)', downloadAsset);
-app.post('/api/folders', createFolder);
+app.post('/api/folders', createFolderAEM);
 app.get('/api/token', getToken);
 app.get('/api/get/metadata/:path(*)', listAssetMeta);
 app.get('/api/assets/:path(*)', listAsset);
-app.get('/api/search/metadata', searchAssetsWithMetadata);
+app.get('/api/search/metadata', searchAssetMetadata);
 app.put('/api/update/metadata/:aemPath(*)', updateMetadata);
-app.post('/api/upload/:targetFolder(*)', upload.array('files'), uploadImage);
+app.post('/api/upload/:targetFolder(*)', upload.array('files'), uploadImageAEM);
 
 // Iniciar el servidor
 app.listen(port, () => {
